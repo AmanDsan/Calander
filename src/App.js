f387691022da02f35d788dccc44eaee4b20cbdf5
AmanDsan <dubeyaman063@gmail.com>
2023-05-05 10:33:04 +0530

    Calander

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
-function App() {
+const Calendar = () => {
+  const [date, setDate] = useState(new Date());
+
+  const handlePrevMonth = () => {
+    setDate(
+      (prevDate) => new Date(prevDate.getFullYear(), prevDate.getMonth() - 1, 1)
+    );
+  };
+
+  const handleNextMonth = () => {
+    setDate(
+      (prevDate) => new Date(prevDate.getFullYear(), prevDate.getMonth() + 1, 1)
+    );
+  };
+
+  const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const daysInMonth = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
+  const firstDayOfMonth = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    1
+  ).getDay();
+
+  const days = [];
+  for (let i = 1; i <= daysInMonth; i++) {
+    days.push(i);
+  }
+
+  const emptyDays = [];
+  for (let i = 0; i < firstDayOfMonth; i++) {
+    emptyDays.push(<div key={`empty-${i}`} className="day empty"></div>);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calendar">
+      <div className="header">
+        <button onClick={handlePrevMonth}>Previous Month</button>
+        <h1>
+          {date.toLocaleString("default", { month: "long", year: "numeric" })}
+        </h1>
+        <button onClick={handleNextMonth}>Next Month</button>
+      </div>
+      <div className="weekdays">
+        {weekdays.map((weekday, index) => (
+          <div key={`weekday-${index}`} className="weekday">
+            {weekday.slice(0, 3)}
+          </div>
+        ))}
+      </div>
+      <div className="days">
+        {emptyDays}
+        {days.map((day) => (
+          <div key={`day-${day}`} className="day">
+            {day}
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Calendar;
